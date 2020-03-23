6ae7f699548586f318118e03ba5633247ce2cc92
Fabio Papa <fabtheman@gmail.com>
2020-03-23 08:29:27 -0700

    Update application to start Pooly

--- mix.exs
+++ mix.exs
@@ -12,22 +12,13 @@ defmodule Pooly.Mixfile do
     ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [
+      applications: [:logger],
+      mod: {Pooly, []}
+    ]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     []
   end
